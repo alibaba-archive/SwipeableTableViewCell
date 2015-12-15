d8e0db5ba15b0654137598481b76b5a84b1317f1
Xin Hong <xin@teambition.com>
2015-12-15 11:31:37 +0800

    initial commit

---
+++ SwipeableTableViewCell/SwipeableTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwipeableTableViewCell.swift
+//  SwipeableTableViewCell
+//
+//  Created by 洪鑫 on 15/12/15.
+//  Copyright © 2015年 Teambition. All rights reserved.
+//
+
+import UIKit
+
+class SwipeableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
